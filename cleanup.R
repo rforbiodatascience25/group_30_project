11f8ee2eb8061aa312916f20f1c0e3f1ea180e80
malaikakhiljee <s245618@dtu.dk>
2025-11-25 10:50:27 +0100

    cleanup fil

--- cleanup.R
+++ cleanup.R
@@ -5,12 +5,8 @@ library(fs)
 # Find alle HTML-filer i R-mappen
 html_files <- dir_ls(here("R"), glob = "*.html")
 
-# Opret results-mappen, hvis den ikke findes
-dir_create(here("results"))
-
 # Flyt hver HTML-fil til results-mappen
 for (f in html_files) {
   dest <- here("results", path_file(f))
   file_move(f, dest)
-  message("Moved: ", f, " -> ", dest)
 }
